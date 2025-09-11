92610bf25ea9084490f26e84d894ba7c71c3d465
anushkagaur22 <150274027+anushkagaur22@users.noreply.github.com>
2025-09-11 15:36:03 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    "faded": "https://www.youtube.com/watch?v=60ItHLz5WEA",
+    "shapeofyou": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "senorita": "https://www.youtube.com/watch?v=Pkh8UtuejGw",
+    "cheapthrills": "https://www.youtube.com/watch?v=nYh-n7EOtMA"
+}
+
